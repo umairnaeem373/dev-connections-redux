b4b86a10613fbab26ddcb39a8f040c20260de736
umairnaeem373 <umairnaeem373@gmail.com>
2024-09-10 01:33:49 +0500

    Added Photos to the Posts

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { editProfile, getUser } from "../Actions/Actions";
 import { useDispatch, useSelector } from "react-redux";
-import { getStorage  } from "firebase/storage";
-import {fbase} from '../firebase'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { fbase } from "../firebase";
 
 function Posts() {
-  
-  const storage = getStorage(fbase)
-  console.log(fbase,storage,'swipe wipe')
-  
+  const storage = getStorage(fbase);
+  console.log(fbase, storage, "swipe wipe");
+
   const [Inp, setInp] = useState({});
+  const [progress, setProgress] = useState(0);
   const dispatch = useDispatch();
   const State = useSelector((e) => e.single);
+
+  const { media } = Inp;
+  console.log(State, "Lindaaaaaaa");
 
   const user = JSON.parse(localStorage.getItem("user"));
 
@@ -21,19 +29,44 @@ function Posts() {
   }, []);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInp({ ...Inp, [name]: value });
+    const { name, value, type } = e.target;
+    type === "file"
+      ? setInp({ ...Inp, [name]: e.target.files[0] })
+      : setInp((Inp) => ({ ...Inp, [name]: value }));
   };
-
-  console.log(Inp);
 
   const handleSubmit = () => {
-    State.user.posts
-      ? dispatch(editProfile(user.id, { posts: [...State.user.posts, Inp] }))
-      : dispatch(editProfile(user.id, { posts: [Inp] }));
-  };
+    if (media) {
+      const storageRef = ref(storage, `myImages/${media.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, media);
 
-  console.log(State, "ppp");
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          var progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            State.user.posts
+              ? dispatch(
+                  editProfile(user.id, {
+                    posts: [...State.user.posts, { ...Inp, media: url }],
+                  })
+                )
+              : dispatch(
+                  editProfile(user.id, { posts: [{ ...Inp, media: url }] })
+                );
+          });
+        }
+      );
+    }
+  };
 
   return (
     <div className="mt-16 p-16">
@@ -43,13 +76,13 @@ function Posts() {
       <h1 className="text-3xl text-left py-2  flex">
         This is where you can view and create posts.
       </h1>
-      <div className="flex">
+      <div className="grid border p-2 grid-cols-1 gap-2 sm:grid-cols-3">
         <input
           onChange={handleChange}
           type="text"
           name="title"
           id="small-input"
-          className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500"
         />
         <input
           onChange={handleChange}
@@ -60,10 +93,11 @@ function Posts() {
         <button
           onClick={handleSubmit}
           type="button"
-          className="inline-flex items-center px-4 py-2  ms-4 text-sm font-medium text-gray-900 bg-orange-400 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          className="inline-flex justify-center max-w-[100px] items-center px-4 py-2 text-sm font-medium text-gray-900 bg-orange-400 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
         >
           Post
         </button>
+        {media && media.name} {progress > 0 && `${progress}% uploaded`}
       </div>
 
       {State.loading === true ? (
@@ -71,8 +105,14 @@ function Posts() {
       ) : (
         State.user?.posts?.map((post, index) => {
           return (
-            <div key={index}>
-              <h1 className="text-black">{post}</h1>
+            <div
+              className="flex flex-wrap justify-center items-center flex-col "
+              key={index}
+            >
+              {post.title && (
+                <h1 className="text-gray-500 border">{post.title}</h1>
+              )}
+              {post.media && <img src={post.media} alt="post.media" />}
             </div>
           );
         })
